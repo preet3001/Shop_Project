6f976a0f03ad4194512ae2823383ee299250000d
Preet Singh <preetsingh@Preets-MacBook-Pro.local>
2022-01-09 17:41:51 +0530

    changed path using routing helper function

--- routes/page_not_found.js
+++ routes/page_not_found.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const path = require('path');
+const rootDir = require('../util/path')
 const router = express.Router();
 
 router.use('/',(req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname,'../','views','404.html'));
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'));
 });
 
 module.exports = router;
