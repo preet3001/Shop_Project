35e78722cd787c25d3b3610a0c1fa775f86ac9a0
Preet Singh <preetsingh@Preets-MacBook-Pro.local>
2022-01-09 17:28:54 +0530

    added status code to page not found file

--- routes/page_not_found.js
+++ routes/page_not_found.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.use('/',(req,res,next) => {
-    res.sendFile(path.join(__dirname,'../','views','404.html'));
+    res.status(404).sendFile(path.join(__dirname,'../','views','404.html'));
 });
 
 module.exports = router;
