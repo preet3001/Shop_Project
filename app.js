e19c712a4673b8cf39a8e6b3e5516a52b345428b
Preet Singh <preetsingh@Preets-MacBook-Pro.local>
2022-01-09 17:11:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const  bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+const pageNotFound = require('./routes/page_not_found');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/admin',adminRoute);
+app.use(shopRoute);
+app.use(pageNotFound);
+
+
+const PORT = 3000;
+app.listen(3000);
